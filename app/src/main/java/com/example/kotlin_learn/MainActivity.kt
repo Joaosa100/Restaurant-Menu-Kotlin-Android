2a4812f0641fb21ebe447f2364f7d08443e5dff2
Joaosa100 <joaovitorsa100@gmail.com>
2024-03-12 03:50:37 -0300

    add main button navigation

--- app/src/main/java/com/example/kotlin_learn/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_learn/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.kotlin_learn
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.kotlin_learn.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.accessMenu.setOnClickListener{
+            val goToMenu = Intent(this, Menu::class.java)
+            startActivity(goToMenu)
+        }
+
     }
 }
