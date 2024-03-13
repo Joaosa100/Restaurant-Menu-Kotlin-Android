070b0550cebe0b64fb78c2bca8a2f88939418052
Joaosa100 <joaovitorsa100@gmail.com>
2024-03-12 21:42:35 -0300

    finished navigation

--- app/src/main/java/com/example/kotlin_learn/Menu.kt
+++ app/src/main/java/com/example/kotlin_learn/Menu.kt
@@ -1,20 +1,32 @@
 package com.example.kotlin_learn
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.kotlin_learn.databinding.ActivityMenuBinding
 
 class Menu : AppCompatActivity() {
+    private lateinit var binding: ActivityMenuBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMenuBinding.inflate(layoutInflater)
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_menu)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.backButton.setOnClickListener {
+            val goToFirstView = Intent(this, MainActivity::class.java)
+            startActivity(goToFirstView)
         }
     }
 }
