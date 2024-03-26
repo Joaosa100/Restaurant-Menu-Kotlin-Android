055ab848eda44f1f7c1fc5577dba1f0d871d8278
Joaosa100 <joaovitorsa100@gmail.com>
2024-03-26 01:09:09 -0300

    add cart and first back end information

--- app/src/main/java/com/example/kotlin_learn/MenuHamb.kt
+++ app/src/main/java/com/example/kotlin_learn/MenuHamb.kt
@@ -11,11 +11,8 @@ import com.example.kotlin_learn.databinding.ActivityMenuHambBinding
 class MenuHamb : AppCompatActivity() {
     private lateinit var binding: ActivityMenuHambBinding
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMenuHambBinding.inflate(layoutInflater)
-
-
 
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -26,12 +23,47 @@ class MenuHamb : AppCompatActivity() {
             insets
         }
 
+        binding.hambBasicao.setOnClickListener {
+            val nomeProduto = getString(R.string.hambbasicao)
+            val tipoProduto =  getString(R.string.hamb)
+            val precoProduto = getString(R.string.hambbasicaoprice)
+            abrirCarrinho(nomeProduto, tipoProduto, precoProduto)
+        }
+
+        binding.bigPicanha.setOnClickListener {
+            val nomeProduto = getString(R.string.hambBigPicanha)
+            val tipoProduto =  getString(R.string.hamb)
+            val precoProduto = getString(R.string.hambbigpicanhaprice)
+            abrirCarrinho(nomeProduto, tipoProduto, precoProduto)
+        }
+
+        binding.doublePicanha.setOnClickListener {
+            val nomeProduto = getString(R.string.hambDoublePicanha)
+            val tipoProduto =  getString(R.string.hamb)
+            val precoProduto = getString(R.string.hambdoublepicanhaprice)
+            abrirCarrinho(nomeProduto, tipoProduto, precoProduto)
+        }
+
+        binding.smokedClassic.setOnClickListener {
+            val nomeProduto = getString(R.string.hambSmokedClassic)
+            val tipoProduto =  getString(R.string.hamb)
+            val precoProduto = getString(R.string.hambsmokedclassicprice)
+            abrirCarrinho(nomeProduto, tipoProduto, precoProduto)
+        }
+
 
         binding.backButton.setOnClickListener {
             val goBackToMenu = Intent(this, Menu::class.java)
             startActivity(goBackToMenu)
         }
-
-
     }
+    private fun abrirCarrinho(nomeProduto: String, tipoProduto: String, precoProduto: String) {
+        // Abre a atividade do carrinho e envia o nome e o preço do produto selecionado
+        val intent = Intent(this, CarrinhoActivity::class.java)
+        intent.putExtra("nome_produto", nomeProduto)
+        intent.putExtra("tipo_produto", tipoProduto)
+        intent.putExtra("preco_produto", precoProduto)
+        startActivity(intent)
+    }
+
 }
